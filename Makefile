73c4860f2e3a70b6a24eadd018f45e117a0f5d9f
Banes <86240550+Banes-dev@users.noreply.github.com>
2023-11-07 15:03:26 +0100

    Add files via upload

---
+++ Makefile
@@ -0,0 +1,28 @@
+CC = cc
+
+CFLAGS = -Wall -Wextra -Werror
+
+NAME = libftget_count_next_line.a
+
+FUNC =	get_next_line.c get_next_line_utils.c
+
+OBJS = ${FUNC:.c=.o} 
+
+
+.c.o:
+		${CC} ${CFLAGS} -c $< -o ${<:.c=.o}
+
+${NAME}:	${OBJS}
+		ar -rsc ${NAME} ${OBJS}
+
+all: 	${NAME}
+
+clean:	
+		rm -f ${OBJS}
+
+fclean:	clean
+		rm -f ${NAME}
+
+re:	fclean all
+
+.PHONY: all clean fclean re
---
+++ get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ehay <ehay@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/07 13:51:09 by ehay              #+#    #+#             */
+/*   Updated: 2023/11/07 14:38:56 by ehay             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFFER_SIZE 1024
+
+# include <stdio.h>           // printf
+# include <unistd.h>          // write
+# include <string.h>          // fonction string
+# include <stdlib.h>          // malloc()
+# include <stddef.h>          // size_t
+# include <stdarg.h>          // fonction va_
+
+// prototype
+
+#endif
